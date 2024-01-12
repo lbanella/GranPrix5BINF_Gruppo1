c01525cad7c6154fcca7feb9f671c8c71f68b052
MarcoMoro16 <marcomoronimarco@gmail.com>
2024-01-12 15:29:30 +0100

    secondo caricamento di prova

--- src/main/java/com/mycompany/granprix5binf/GestoreGara.java
+++ src/main/java/com/mycompany/granprix5binf/GestoreGara.java
@@ -9,7 +9,6 @@ package com.mycompany.granprix5binf;
  * @author Utente
  */
 public class GestoreGara{
-
     public static void main(String[] args) {
         System.out.println("Hello ciao!");
     }
