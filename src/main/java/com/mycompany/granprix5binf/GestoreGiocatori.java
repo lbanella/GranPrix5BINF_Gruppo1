bd97243b7ba1b1633cbb1dc608e442885a31e00f
lbanella <banella.lorenzo@gmail.com>
2024-02-16 16:11:44 +0100

    Classe GestoreGiocatori completata

--- src/main/java/com/mycompany/granprix5binf/GestoreGiocatori.java
+++ src/main/java/com/mycompany/granprix5binf/GestoreGiocatori.java
@@ -2,6 +2,8 @@ package com.mycompany.granprix5binf;
 
 import java.io.*;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,31 +17,125 @@ public class GestoreGiocatori {
 	private static final String nomeFileListaGiocatori = "dati.bin";
 
 	public GestoreGiocatori() {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+            this.listaGiocatori = new ArrayList<>();
 	}
 
 	public void addGiocatore(Giocatore giocatore) {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+		listaGiocatori.add(giocatore);
 	}
 
 	public void esportaGiocatori() {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+           try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(nomeFileListaGiocatori))) {
+            oos.writeObject(listaGiocatori);
+            } catch (IOException e) {
+            System.err.println("Errore durante l'esportazione dei giocatori: " + e.getMessage());
+            }
 	}
 
 	public void importaGiocatori() {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(nomeFileListaGiocatori))) {
+                this.listaGiocatori = (ArrayList<Giocatore>) ois.readObject();
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(GestoreGiocatori.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException | ClassNotFoundException ex) {
+                Logger.getLogger(GestoreGiocatori.class.getName()).log(Level.SEVERE, null, ex);
+            }
 	}
 
-	public boolean formUtente() {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
-	}
+	public  boolean formUtente() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("--------------------------------------------------");
+            System.out.println("|               GrandPrix Simulatore             |");
+            System.out.println("--------------------------------------------------");
+            System.out.println("| 1) Registrarti                                 |");
+            System.out.println("| 2) Login                                       |");
+            System.out.println("--------------------------------------------------");
+            System.out.println("| 0) Uscita                                      |");
+            System.out.println("--------------------------------------------------");
+            System.out.print("Scelta: ");
+
+            int scelta = scanner.nextInt();
+            scanner.nextLine(); // Consuma il carattere di nuova linea residuo
+
+            boolean registrato =false;
+            boolean autenticato = false;
+
+
+            switch (scelta) {
+                case 1:
+                        importaGiocatori();
+                        registrato =effettuaRegistrazione();
+                        autenticato = effettuaLogin();
+                        break;
+                case 2:
+                        importaGiocatori();
+                        autenticato = effettuaLogin();
+                        break;
+                default:
+                        System.out.println("ATTENZIONE!!! Scelta errata");
+            }
+
+            return autenticato;
+        }
         
         public boolean effettuaRegistrazione() {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+            boolean registrato=false;
+            Giocatore nuovoGiocatore;
+            do{
+                nuovoGiocatore = new Giocatore();
+                // verifico se l'username è già presente, nell array lista giocatori
+                boolean usernamePresente = false;
+                for (Giocatore giocatore : listaGiocatori) {
+                    if (giocatore.getUsername().equals(nuovoGiocatore.getUsername())) {
+                        usernamePresente = true;
+                        System.out.println("Username gia in uso. Riprova");
+                        break;
+                    }
+                }
+                if (!usernamePresente) {
+                    registrato=true;
+                }   
+            }while(!registrato);
+                
+            addGiocatore(nuovoGiocatore);
+            esportaGiocatori();
+            System.out.println("Registrazione completata. Ora puoi effettuare il login.");
+            return registrato;
 	}
 
 	public boolean effettuaLogin() {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+             Scanner scanner = new Scanner(System.in);
+            boolean autenticato = false;
+            do{
+                System.out.println("Inserisci l'username (oppure '0' per tornare indietro)");
+                String username = scanner.nextLine();
+
+                if (username.equals("0")) {
+                    System.out.println("Sei tornato indietro, rieffettua il login.");
+                    break;
+                }
+            
+                System.out.println("Inserisci il verme");
+                String verme = scanner.nextLine().toUpperCase();
+                // Creo un cifrario che mi servirà a fare il confronto tra le password 
+                Cifrario cifrario = new Cifrario(verme);
+                System.out.println("Inserisci la password");
+                String password = cifrario.cifra(scanner.nextLine().toUpperCase());
+                boolean utenteTrovato = false;
+                for (Giocatore g :listaGiocatori){
+                    if (username.equals(g.getUsername()) && password.equals(g.getPassword())) {
+                        utenteTrovato=true;
+                    } 
+                }
+                
+                if(utenteTrovato){
+                    autenticato = true;
+                }else{
+                    System.out.println("Credenziali sbagliate, riprova!");
+                }
+            } while (!autenticato);
+            
+        return autenticato;
 	}
 
 }
